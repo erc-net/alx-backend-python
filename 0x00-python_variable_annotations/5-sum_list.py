13f220615945c0b06f33ec345c5044c4a6fd4f61
erc-net <erictaruwinga0827@gmail.com>
2023-07-06 09:20:40 -0400

    python

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,10 +2,11 @@
 """
 Complex types - list of floats
 """
-
-
-from typing import list
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    return a sum of all nums inside a list
+    """
     return sum(input_list)
